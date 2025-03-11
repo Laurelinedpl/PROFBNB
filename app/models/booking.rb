341af7ee5af46a5737c632f0e98beebaba529c30
Kevin Saison <kevin.saison91@gmail.com>
2025-03-11 12:48:22 +0100

    Add validations to models & seed database

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :teacher
   belongs_to :user
+
+  validates :date, presence: true
+  validates :status, presence: true, inclusion: { in: %w[pending confirmed cancelled] }
+  validates :teacher_id, presence: true
+  validates :user_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Clear the database
+Booking.destroy_all
+User.destroy_all
+Teacher.destroy_all
+
+# Create 5 users
+users = []
+users << User.create(email: "kevin@example.com", password: "kevinsaison", first_name: "Kevin", last_name: "Saison")
+users << User.create(email: "baptiste@example.com", password: "baptistecasagrande", first_name: "Baptiste", last_name: "Casagrande")
+users << User.create(email: "laureline@example.com", password: "laurelinedesplanches", first_name: "Laureline", last_name: "Desplanches")
+users << User.create(email: "lotfi@example.com", password: "lotfibensmail", first_name: "Lotfi", last_name: "Bensmail")
+users << User.create(email: "anna@example.com", password: "annaboulin", first_name: "Anna", last_name: "Boulin")
+
+puts "Created #{User.count} users"
+
+# Create 5 teachers
+teachers = []
+teachers << Teacher.create(first_name: "Maellie", last_name: "Poussier")
+teachers << Teacher.create(first_name: "Benoit", last_name: "Moret")
+teachers << Teacher.create(first_name: "Erika", last_name: "Fonseca")
+teachers << Teacher.create(first_name: "Eva", last_name: "Touboulic")
+teachers << Teacher.create(first_name: "Jerome", last_name: "Tan")
+
+puts "Created #{Teacher.count} teachers"
+
+# Create 5 bookings
+5.times do |i|
+  Booking.create(date: Date.today, status: "pending", user: users[i], teacher: teachers[i])
+end
+
+puts "Created #{Booking.count} bookings"
+
+puts "Seeding complete"
